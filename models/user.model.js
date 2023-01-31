d32bfa995bb313e6c0391744c236285ee4519e5b
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2023-01-31 23:41:37 +0530

    feat: add referalcode to usermodel

--- models/user.model.js
+++ models/user.model.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
-
+const {nanoid} = require('nanoid');
 const userSchema= new mongoose.Schema({
     email:{type:String, required:true},
     password:{type:String, required:true},
     name:{type:String,required:true},
     userId:{type:String,required:true},
+    referalCode:{type:String, required:true,default:nanoid(6)},
     role:{type:String,default:'user'}
 
 },{timestamps:true});
